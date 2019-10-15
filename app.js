4c34080b4ccf5658db9a25f41d9d1c3dafc5ddd8
ahmadnazif <ahmadnazif@gmail.com>
2019-10-15 11:19:47 +0800

    init

---
+++ app.js
@@ -0,0 +1,22 @@
+const api = require('RestApiSender.js');
+
+listAllFnb();
+
+async function add(fnbName, fnbType) {
+    try{
+    var resp = await api.addFnbAsync(fnbName, fnbType);    
+    console.log(resp);
+    }
+    catch(error){
+        console.log('Ex: '+resp.error);
+    }
+}
+
+async function listAllFnb(){
+    try {
+        var resp = await api.listAllFnbAsync();
+        console.log(resp.responseContentString);
+    } catch (error) {
+        console.log('Ex: '+resp.error);
+    }
+}
